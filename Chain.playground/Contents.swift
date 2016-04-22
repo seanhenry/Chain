6ce924525603e18d0e85c4f35f61525bc3f86a3e
Sean Henry <hello@seanhenry.codes>
2016-04-22 22:07:29 +0100

    Updates Playground

--- Chain.playground/Contents.swift
+++ Chain.playground/Contents.swift
@@ -3,64 +3,82 @@
 import UIKit
 import Chain
 
-class IntGenerator: Link<(), Int> {
+class IntGenerator<Ignored>: Link<Ignored, Int> {
 
-    override func main(done: () -> ()) {
-        result = 42
-        done()
+    override func run() {
+        finish(result: 42)
     }
 }
 
 class IntToString: Link<Int, String> {
 
-    override func main(done: () -> ()) {
-        result = String(initial)
-        done()
+    override func run() {
+        finish(result: String(initialValue))
     }
 }
 
-class Print: Link<String, ()> {
-
-    override func main(done: () -> ()) {
-        print(initial ?? "nil")
-        done()
-    }
-}
-
-class Add: Link<Int, Int> {
+class Add: PassiveLink<Int> {
 
     let value: Int
     init(_ value: Int) {
         self.value = value
     }
 
-    override func main(done: () -> ()) {
-        result = (initial ?? -10000) + value
-        done()
+    override func run() {
+        finish(result: initialValue + value)
     }
 }
 
 class Delay<PassedType>: PassiveLink<PassedType> {
 
-    override func main(done: () -> ()) {
+    override func run() {
         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(0.5) * Int64(NSEC_PER_SEC))
-        dispatch_after(time, dispatch_get_main_queue(), done)
+        dispatch_after(time, dispatch_get_main_queue(), finish)
     }
 }
 
 class JustPrint<PassedType>: PassiveLink<PassedType> {
 
-    override func main(done: () -> ()) {
+    override func run() {
         print("Just printing")
-        done()
+        finish()
     }
 }
 
-Chain(IntGenerator()).then(IntToString()).finally { result in
-    print("block " + result)
+Chain(IntGenerator<Void>()).then(IntToString()).finally { result in
+    print("IntToString: \(result)")
 }.run()
-Chain(IntGenerator()).then(IntToString()).finally(Print()).run()
-Chain(IntGenerator()).then(JustPrint()).then(IntToString()).finally(Print()).run()
-Chain(IntGenerator()).then(Add(50)).then(IntToString()).then(JustPrint()).then(Delay()).finally(Print()).run()
+Chain(IntGenerator<Void>()).then(Add(50)).then(IntToString()).finally { result in
+    print("Passive Link: \(result)")
+}.run()
+Chain(IntGenerator<Void>()).then(IntToString()).then(Delay()).finally { result in
+    print("Asynchronous: \(result)")
+}.run()
+
+enum Error: ErrorType {
+    case Error1
+}
+
+class FailLink<Ignored>: Link<Ignored, String> {
+
+    override func run() {
+        finish(error: Error.Error1)
+    }
+}
+
+class SuccessLink<Ignored>: Link<Ignored, String> {
+
+    override func run() {
+        finish(result: "yay!")
+    }
+}
+
+Chain(FailLink<Void>()).then(SuccessLink()).finally { result in
+    print("Failure: \(result)")
+}.run()
+
+Chain(SuccessLink<()>()).then(SuccessLink()).finally { result in
+    print("Success: \(result)")
+}.run()
 
 NSRunLoop.mainRunLoop().runUntilDate(NSDate(timeIntervalSinceNow: 1))
