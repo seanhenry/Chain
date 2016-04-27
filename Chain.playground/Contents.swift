e36e05b6915affb4d815da3ea23fc383e66fcdad
Sean Henry <hello@seanhenry.codes>
2016-04-27 23:04:07 +0100

    Updates playground and README

--- Chain.playground/Contents.swift
+++ Chain.playground/Contents.swift
@@ -1,35 +1,66 @@
-//: # Build the Chain framework first
-
-import UIKit
 import Chain
+/*: 
+ # Chain
+ A Swift library for linking tasks together and passing data between those tasks.
 
-class IntGenerator<Ignored>: Link<Ignored, Int> {
+ ## Simple Example
+ Suppose you have asked the user for an integer value and want to perform some multiplication on that number.
+*/
+class MultiplyBy: Link<Int, Int> {
 
-    override func run() {
-        finish(result: 42)
-    }
-}
-
-class IntToString: Link<Int, String> {
-
-    override func run() {
-        finish(result: String(initialValue))
-    }
-}
-
-class Add: PassiveLink<Int> {
-
-    let value: Int
-    init(_ value: Int) {
-        self.value = value
+    let factor: Int
+    init(_ factor: Int) {
+        self.factor = factor
     }
 
     override func run() {
-        finish(result: initialValue + value)
+        let result = initialValue * factor
+        finish(result: result)
     }
 }
 
-class Delay<PassedType>: PassiveLink<PassedType> {
+Chain(MultiplyBy(10), initialValue: 15).finally { result in
+    print(result) // => 150
+}.run()
+/*:
+ Now suppose you want to convert that number to words. You can easily link two tasks together providing the output type of the previous link matches the input type of the next link.
+ */
+class IntToWords: Link<Int, String> {
+
+    override func run() {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .SpellOutStyle
+        let result = formatter.stringFromNumber(initialValue)!
+        finish(result: result)
+    }
+}
+
+Chain(MultiplyBy(10), initialValue: 15)
+    .then(IntToWords())
+    .finally { result in
+    print(result) // => "one hundred fifty"
+}.run()
+/*:
+ Sometimes tasks are not interested in the input or output. Here we can create a passive link which will simply pass on the value from one link to the next.
+ */
+class Boast<IgnoredType>: PassiveLink<IgnoredType> {
+
+    override func run() {
+        print("I will now convert your number into text.")
+        finish()
+    }
+}
+
+Chain(MultiplyBy(10), initialValue: 15)
+    .then(Boast()) // => "I will now convert your number into text."
+    .then(IntToWords())
+    .finally { result in
+    print(result) // => "one hundred fifty"
+}.run()
+/*:
+ Tasks can be asynchronous, like network requests or animations.
+ */
+class PauseForEffect<IgnoredType>: PassiveLink<IgnoredType> {
 
     override func run() {
         let time = dispatch_time(DISPATCH_TIME_NOW, Int64(0.5) * Int64(NSEC_PER_SEC))
@@ -37,51 +68,43 @@ class Delay<PassedType>: PassiveLink<PassedType> {
     }
 }
 
-class JustPrint<PassedType>: PassiveLink<PassedType> {
-
-    override func run() {
-        print("Just printing")
-        finish()
-    }
-}
-
-Chain(IntGenerator<Void>()).then(IntToString()).finally { result in
-    print("IntToString: \(result)")
+Chain(MultiplyBy(10), initialValue: 15)
+    .then(Boast())
+    .then(PauseForEffect()) // IntToWords is not called until the pause is finished.
+    .then(IntToWords())
+    .finally { result in
+    print(result)
 }.run()
-Chain(IntGenerator<Void>()).then(Add(50)).then(IntToString()).finally { result in
-    print("Passive Link: \(result)")
-}.run()
-Chain(IntGenerator<Void>()).then(IntToString()).then(Delay()).finally { result in
-    print("Asynchronous: \(result)")
-}.run()
-Chain(IntToString(), initialValue: 999).finally { result in
-    print("Initial Value: \(result)")
-}.run()
-
+/*:
+ Tasks can fail. And when they do the error is passed to the end of the Chain and the next links are ignored.
+ */
 enum Error: ErrorType {
-    case Error1
+    case NumberTooHigh
 }
 
-class FailLink<Ignored>: Link<Ignored, String> {
+class ICanOnlyCountTo: Link<Int, Int> {
+
+    let max: Int
+    init(_ max: Int) {
+        self.max = max
+    }
 
     override func run() {
-        finish(error: Error.Error1)
+        if initialValue < max {
+            finish(result: initialValue)
+        } else {
+            finish(error: Error.NumberTooHigh)
+        }
     }
 }
 
-class SuccessLink<Ignored>: Link<Ignored, String> {
-
-    override func run() {
-        finish(result: "yay!")
-    }
-}
-
-Chain(FailLink<Void>()).then(SuccessLink()).finally { result in
-    print("Failure: \(result)")
+Chain(MultiplyBy(10), initialValue: 15)
+    .then(Boast())
+    .then(PauseForEffect())
+    .then(ICanOnlyCountTo(100))
+    .then(IntToWords())
+    .finally { result in
+    print(result) // => Error.NumberTooHigh
 }.run()
 
-Chain(SuccessLink<()>()).then(SuccessLink()).finally { result in
-    print("Success: \(result)")
-}.run()
-
-NSRunLoop.mainRunLoop().runUntilDate(NSDate(timeIntervalSinceNow: 1))
+NSRunLoop.mainRunLoop().runUntilDate(NSDate(timeIntervalSinceNow: 2))
--- README.md
+++ README.md
@@ -1,1 +1,120 @@
+
 # Chain
+A Swift library for linking tasks together and passing data between those tasks.  
+## Playground
+See the embedded playground for a working example. Open the workspace and build the framework before running the playground.
+
+## Simple Example
+Suppose you have asked the user for an integer value and want to perform some multiplication on that number.  
+
+```
+class MultiplyBy: Link<Int, Int> {
+
+    let factor: Int
+    init(_ factor: Int) {
+        self.factor = factor
+    }
+
+    override func run() {
+        let result = initialValue * factor
+        finish(result: result)
+    }
+}
+
+Chain(MultiplyBy(10), initialValue: 15).finally { result in
+    print(result) // => 150
+}.run()
+```
+
+Now suppose you want to convert that number to words. You can easily link two tasks together providing the output type of the previous link matches the input type of the next link.
+
+```
+class IntToWords: Link<Int, String> {
+
+    override func run() {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .SpellOutStyle
+        let result = formatter.stringFromNumber(initialValue)!
+        finish(result: result)
+    }
+}
+
+Chain(MultiplyBy(10), initialValue: 15)
+    .then(IntToWords())
+    .finally { result in
+    print(result) // => "one hundred fifty"
+}.run()
+```
+
+Sometimes tasks are not interested in the input or output. Here we can create a passive link which will simply pass on the value from one link to the next.
+
+```
+class Boast<IgnoredType>: PassiveLink<IgnoredType> {
+
+    override func run() {
+        print("I will now convert your number into text.")
+        finish()
+    }
+}
+
+Chain(MultiplyBy(10), initialValue: 15)
+    .then(Boast()) // => "I will now convert your number into text."
+    .then(IntToWords())
+    .finally { result in
+    print(result) // => "one hundred fifty"
+}.run()
+```
+
+Tasks can be asynchronous, like network requests or animations.
+
+```
+class PauseForEffect<IgnoredType>: PassiveLink<IgnoredType> {
+
+    override func run() {
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(0.5) * Int64(NSEC_PER_SEC))
+        dispatch_after(time, dispatch_get_main_queue(), finish)
+    }
+}
+
+Chain(MultiplyBy(10), initialValue: 15)
+    .then(Boast())
+    .then(PauseForEffect()) // IntToWords is not called until the pause is finished.
+    .then(IntToWords())
+    .finally { result in
+    print(result)
+}.run()
+```
+
+Tasks can fail. And when they do the error is passed to the end of the Chain and the next links are ignored.
+
+```
+enum Error: ErrorType {
+    case NumberTooHigh
+}
+
+class ICanOnlyCountTo: Link<Int, Int> {
+
+    let max: Int
+    init(_ max: Int) {
+        self.max = max
+    }
+
+    override func run() {
+        if initialValue < max {
+            finish(result: initialValue)
+        } else {
+            finish(error: Error.NumberTooHigh)
+        }
+    }
+}
+
+Chain(MultiplyBy(10), initialValue: 15)
+    .then(Boast())
+    .then(PauseForEffect())
+    .then(ICanOnlyCountTo(100))
+    .then(IntToWords())
+    .finally { result in
+    print(result) // => Error.NumberTooHigh
+}.run()
+
+```
